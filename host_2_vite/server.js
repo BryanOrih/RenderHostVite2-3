91c8f7502d6d4739605525bc93abcdc2904806a3
BryanOrih <129115358+BryanOrih@users.noreply.github.com>
2023-06-15 16:27:27 -0700

    finished 2

---
+++ host_2_vite/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const server = express()
+const port = 3005
+
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
+
+server.use(express.static(path.join(__dirname, '/dist')))
+
+server.listen(port, ()=>{
+    console.log("success!!!");
+})
